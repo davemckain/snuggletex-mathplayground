2eb5d9e555a57ec111f3a5bd4fc1f96bcd316386
David McKain <david.mckain@ed.ac.uk>
2011-03-03 12:01:21 +0000

    Added ASCIIMathParserBrowserUtilities, which will house some of the helpers used in the demo and in other places. Also added indentation function to this, which can be useful in demos.

--- src/main/webapp/ASCIIMathParserDemo.jsp
+++ src/main/webapp/ASCIIMathParserDemo.jsp
@@ -14,67 +14,30 @@ All Rights Reserved
 <c:set var="pageId" value="asciiMathParserDemo" />
 <c:set var="headStuff">
   <script type="text/javascript" src="includes/ASCIIMathParser.js"></script>
+  <script type="text/javascript" src="includes/ASCIIMathParserBrowserUtilities.js"></script>
 </c:set>
 
 <%@ include file="/WEB-INF/jsp/includes/header.jspf" %>
 
 <script type="text/javascript">//<![CDATA[
 
-function createXMLDocument() {
-  var doc;
-  if (document.implementation && document.implementation.createDocument) {
-    /* Gecko, Webkit, Opera */
-    doc = document.implementation.createDocument("", "", null);
-  }
-  else {
-    try {
-      /* Internet Explorer */
-      doc = new ActiveXObject("Microsoft.XMLDOM");
-    }
-    catch (e) {
-      alert("I don't know how to create a DOM Document in this browser");
-    }
-  }
-  return doc;
-}
-
-function serializeXMLNode(node) {
-  var xml;
-  try {
-    /* Gecko, Webkit, Opera */
-    var serializer = new XMLSerializer();
-    xml = serializer.serializeToString(node);
-  }
-  catch (e) {
-    try {
-      /* Internet Explorer */
-      xml = node.xml;
-    }
-    catch (e) {
-      alert("I don't know how to serialize XML in this browser");
-    }
-  }
-  return xml;
-}
-
 function runDemo() {
-  var asciiMathInput = document.forms[0].asciiMathInput.value;
+    var asciiMathInput = document.forms[0].asciiMathInput.value;
 
-  var domDocument = createXMLDocument();
-  var asciiMathParser = new ASCIIMathParser(domDocument);
-  var asciiMathParser2 = new ASCIIMathParser(domDocument);
-  var mathElement = asciiMathParser.parseASCIIMathInput(asciiMathInput);
-  var mathMLString = serializeXMLNode(mathElement);
+    var domDocument = ASCIIMathParserBrowserUtilities.createXMLDocument();
+    var asciiMathParser = new ASCIIMathParser(domDocument);
+    var mathElement = asciiMathParser.parseASCIIMathInput(asciiMathInput);
+    var mathMLString = ASCIIMathParserBrowserUtilities.serializeXMLNode(mathElement);
 
-  alert("MathML from ASCIIMath:\n" + mathMLString);
-  return false;
+    alert("MathML from ASCIIMath:\n" + ASCIIMathParserBrowserUtilities.indentMathMLString(mathMLString));
+    return false;
 }
 
 //]]></script>
 
 <h2>ASCIIMath Parser Demo</h2>
 
-<form action="ASCIIMathParserDemo.jsp" class="input">
+<form action="ASCIIMathParserDemo.jsp" class="input" onsubmit="return runDemo()">
   <div class="inputBox">
     <input id="asciiMathInput" name="asciiMathInput" type="text" value="sinx/cosy">
     <input type="button" value="Parse" onclick="runDemo()">
---
+++ src/main/webapp/includes/ASCIIMathParserBrowserUtilities.js
@@ -0,0 +1,105 @@
+/*
+
+ASCIIMathParserBrowserUtilities.js
+==================================
+
+This contains some helper functions that might be useful if you
+want to use ASCIIMathParser.js in a browser.
+
+Requirements:
+
+ASCIIMathParser.js
+
+---------------------------------------------------------------
+
+Copyright (c) 2011, The University of Edinburgh
+
+This programme is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 2.1 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+(at http://www.gnu.org/licences/lgpl.html) for more details.
+
+---------------------------------------------------------------
+
+*/
+
+ASCIIMathParserBrowserUtilities = {
+
+    createXMLDocument: function() {
+        var doc;
+        if (document.implementation && document.implementation.createDocument) {
+            /* Gecko, Webkit, Opera */
+            doc = document.implementation.createDocument("", "", null);
+        }
+        else {
+            try {
+                /* Internet Explorer */
+                doc = new ActiveXObject("Microsoft.XMLDOM");
+            }
+            catch (e) {
+                throw new Error("I don't know how to create a DOM Document in this browser");
+            }
+        }
+        return doc;
+    },
+
+    serializeXMLNode: function(node) {
+        var xml;
+        try {
+            /* Gecko, Webkit, Opera */
+            var serializer = new XMLSerializer();
+            xml = serializer.serializeToString(node);
+        }
+        catch (e) {
+            try {
+                /* Internet Explorer */
+                xml = node.xml;
+            }
+            catch (e) {
+                throw new Error("I don't know how to serialize XML in this browser");
+            }
+        }
+        return xml;
+    },
+
+    indentMathMLString: function(str) {
+        var indent = arguments[1] || 2;
+        var newline = "\n";
+
+        /* This goes for a simple "split on <" approach, which is fine if the input
+         * is assumed to be well-formed XML. It will fall apart otherwise!
+         */
+        var result = "";
+        var doIndent = function(level) {
+            result += newline;
+            for (var j=0; j<indent * level; j++) {
+                result += ' ';
+            }
+        };
+        var parts = str.split("<");
+        var inTextElement = false;
+        var currentIndentLevel = 0;
+        for (i=1; i<parts.length; i++) {
+            var part = parts[i];
+            if (part.charAt(0)=='/') { /* </element> */
+                if (!inTextElement) { /* </mrow> et al goes on newline */
+                    doIndent(currentIndentLevel);
+                }
+                result += '<' + part;
+                currentIndentLevel--;
+                inTextElement = false;
+            }
+            else { /* <element>content? */
+                doIndent(++currentIndentLevel);
+                result += '<' + part;
+                inTextElement = (part.indexOf(">") < part.length);
+            }
+        }
+        return result;
+    },
+}
